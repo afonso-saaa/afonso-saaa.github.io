bddff1a5f9767634887e4a56ca9af0504a383ce0
Afonso Sá <79218984+afonso-saaa@users.noreply.github.com>
2024-11-14 13:22:09 +0000

    lab8 finalizado

--- lab8/main.js
+++ lab8/main.js
@@ -2,14 +2,19 @@ function showMessageIndex() {
     document.getElementById("hoverText").innerText = "1. Obrigado por passares!";
 }
 
-function resetMessageIndex() {
+const resetMessageIndex = () => {
     document.getElementById("hoverText").innerText = "1. Passa por aqui!";
-}
+};
 
 
-function changeColor(color) {
-    document.getElementById("pintaMe").style.color = color;
-}
+document.querySelectorAll('button[data-color]').forEach((button) => {
+    button.addEventListener('click', () => {
+        const color = button.dataset.color;
+
+        document.getElementById("pintaMe").style.color = color;
+    });
+});
+
 
 function changeBoxColor() {
     const inputBox = document.getElementById("colorfulText");
@@ -17,14 +22,40 @@ function changeBoxColor() {
     inputBox.style.backgroundColor = randomColor;
 }
 
-let count = 0;
+let count = localStorage.getItem("counter") ? parseInt(localStorage.getItem("counter")) : 0;
+document.getElementById("counter").innerText = count;
+
 function incrementCounter() {
     count++;
+    localStorage.setItem("counter", count);
     document.getElementById("counter").innerText = count;
 }
 
 
-function changeBackgroundColor() {
-    const color = document.getElementById("colorInput").value.toLowerCase();
-    document.body.style.backgroundColor = color;
+window.onload = function () {
+    const savedColor = localStorage.getItem("backgroundColor");
+    if (savedColor) {
+        document.body.style.backgroundColor = savedColor;
+    }
 }
+
+function showGreeting() {
+    const name = document.getElementById("userName").value;
+    const age = document.getElementById("userAge").value;
+
+    document.getElementById("greetingMessage").innerText = `Olá, o ${name} tem ${age}!`;
+
+}
+
+function changeBackgroundColor(element) {
+    document.body.style.backgroundColor = element.value;
+}
+
+
+
+let autoCount = 0;
+
+setInterval(function() {
+    autoCount++;
+    document.getElementById("autoCounter").innerText = autoCount;
+}, 1000); 
